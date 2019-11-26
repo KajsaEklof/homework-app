67df4e11de566fc68ad15f2d2289ddc059dabd87
Kajsa1 <kajsaKeklof@gmail.com>
2019-11-26 21:48:16 +0000

    Updates

---
+++ precache-manifest.260beeb3de3072c756fa661862effa9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c572a8d5d4efc1867dd885c4a03ba0",
+    "url": "/homework-app/index.html"
+  },
+  {
+    "revision": "4103106cb672e6016405",
+    "url": "/homework-app/static/css/main.72649fd6.chunk.css"
+  },
+  {
+    "revision": "9f9faac6daf3756790d4",
+    "url": "/homework-app/static/js/2.4e0205ce.chunk.js"
+  },
+  {
+    "revision": "4103106cb672e6016405",
+    "url": "/homework-app/static/js/main.42f252c8.chunk.js"
+  },
+  {
+    "revision": "285dc73a8daf5dc1c3a0",
+    "url": "/homework-app/static/js/runtime-main.ec97dc2a.js"
+  },
+  {
+    "revision": "4a03d3a159502a0b639c2932dce4bf5e",
+    "url": "/homework-app/static/media/3099486.4a03d3a1.jpg"
+  }
+]);
