6a719b81d191893612d1d460da0811af30a65662
Kajsa1 <kajsaKeklof@gmail.com>
2019-11-26 20:54:15 +0000

    Added applicaion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import moment from 'moment';
 import './App.css';
+import Countdown from "./Countdown";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return (
+      <Countdown timeTillDate="12 24 2019, 06:00 am" timeFormat="MM DD YYYY, h:mm a"/>
+    );
+  }
 }
-
+ 
 export default App;
+
